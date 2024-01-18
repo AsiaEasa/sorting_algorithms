b3cadd74d79c0c74a0618b231cd2c4036637d612
AsiaEasa <asiaeasa89@gmail.com>
2024-01-18 13:26:41 -0800

    done

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,43 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - insertion to sort a doubly linked list
+ *
+ * @list: the address of the node
+ *
+ * Return: nothing (void)
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *x, *y, *a;
+
+	if (!list || !*list || !(*list)->next)
+		return;
+	x = (*list)->next;
+	while (x != NULL)
+	{
+		y = x;
+		x = x->next;
+		while (y != NULL && y->prev != NULL)
+		{
+			a = y->prev;
+			if (y->prev->n > y->n)
+			{
+				if (y->next)
+					y->next->prev = a;
+				if (a->prev)
+					a->prev->next = y;
+				a->next = y->next;
+				y->prev = a->prev;
+				a->prev = y;
+				y->next = a;
+				if (!y->prev)
+					*list = y;
+				print_list((const listint_t *)*list);
+			}
+			else
+				y = y->prev;
+		}
+	}
+}
