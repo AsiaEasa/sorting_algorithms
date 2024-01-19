354c03af76b77768362e6fe77ffb0e0032cd99a9
Amedamin <ahmedqryqr@gmail.com>
2024-01-19 02:21:47 -0800

    done

--- sort.h
+++ sort.h
@@ -37,5 +37,6 @@ void cocktail_sort_list(listint_t **list);
 void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker);
 void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker);
 void cocktail_sort_list(listint_t **list);
-
+void S_node_B(listint_t **list, listint_t **Til, listint_t **SH);
+void S_node_A(listint_t **list, listint_t **Til, listint_t **SH);
 #endif
