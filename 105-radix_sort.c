2793821aaf4904be93e75d516685cc612d22b102
AsiaEasa <asiaeasa89@gmail.com>
2024-01-20 09:26:16 -0800

    done

---
+++ 105-radix_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+
+/**
+ * radix_sort - Sort an array of integers in ascending
+ *              order using the radix sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ */
+
+void radix_sort(int *array, size_t size)
+{
+	int l, k, p, x, i;
+
+	l = size;
+	k = size;
+
+	while (l > 0)
+	{
+		for (x = 0; x < l - 1; x++)
+		{
+			if ((array[x] % 10) > (array[x + 1] % 10))
+			{
+				p = array[x];
+				array[x] = array[x + 1];
+				array[x + 1] = p;
+			}
+		}
+		l--;
+	}
+	print_array(array, size);
+
+	while (k > 0)
+	{
+		for (x = 0; x < k - 1; x++)
+		{
+			if ((array[x] % 100) > (array[x + 1] % 100))
+			{
+				p = array[x];
+				array[x] = array[x + 1];
+				array[x + 1] = p;
+			}
+		}
+		k--;
+	}
+	print_array(array, size);
+}
