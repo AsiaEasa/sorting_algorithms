ddeef1b8de8ab09dadd5ca258ef99aeca9066ffa
AsiaEasa <asiaeasa89@gmail.com>
2024-01-22 09:29:01 -0800

    done

--- deck.h
+++ deck.h
@@ -1,6 +1,12 @@
 #ifndef DECK_H
 #define DECK_H
-
+/**
+ * enum kind_e - Enumeration of card suits.
+ * @SPADE: Spades suit.
+ * @HEART: Hearts suit.
+ * @CLUB: Clubs suit.
+ * @DIAMOND: Diamonds suit.
+ */
 typedef enum kind_e
 {
 	SPADE = 0,
